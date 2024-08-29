b3ca9269c0c8d0c8f33257fe59a34b0625800138
karthikeyanrajan21 <149597338+karthikeyanrajan21@users.noreply.github.com>
2024-08-29 13:34:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "karthi" {
   ami           = "ami-0e86e20dae9224db8"
   instance_type = "t2.micro"  
   key_name       = "knlapkey"
-  security_groups = ["sg-04736a6da63f91a5b"]
+  vpc_security_group_ids = ["sg-04736a6da63f91a5b"]
 
   tags = {
     Name = "MyInstance"  
